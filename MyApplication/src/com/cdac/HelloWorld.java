9544cf6f7573a98d3ea378b4919e9e09534e6172
ishaankadam <ishaankadam@192.168.113.123>
2022-12-21 09:49:53 +0530

    Added Goodmorning and Date

--- MyApplication/src/com/cdac/HelloWorld.java
+++ MyApplication/src/com/cdac/HelloWorld.java
@@ -1,12 +1,17 @@
 package com.cdac;
 
+import java.util.Date;
+
 public class HelloWorld {
 
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello World");
-
+		System.out.println("Good Morning Mumbai");
+		Date d = new Date();
+		System.out.println(d);
 	}
 
 }
 // https://github.com/IshaanIJK/eclipse.git
+//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 PAT expires in 30days
