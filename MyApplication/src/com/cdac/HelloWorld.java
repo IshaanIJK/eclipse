37889f3364375222615d751cae312e18cdfc4e35
ishaankadam <ishaankadam@192.168.113.123>
2022-12-21 09:38:35 +0530

    my first commit

---
+++ MyApplication/src/com/cdac/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.cdac;
+
+public class HelloWorld {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World");
+
+	}
+
+}
+// https://github.com/IshaanIJK/eclipse.git
---
+++ MyApplication/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author ishaankadam
+ *
+ */
+module MyApplication {
+}
